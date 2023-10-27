94d8be101673126fd7fef711f6604d8eceedf166
vijeth_simha <vijethsimha71@gmail.com>
2023-10-27 23:19:56 +0530

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          Page rendered on {''}
+          <p> {new Date().toLocaleString()}</p>
+        </footer>
       </body>
     </html>
   );
